29baeacaf61b2d41160cae4104c620baba530973
Saad Ejaz <98383665+Im-Saad@users.noreply.github.com>
2022-03-28 16:54:32 +0530

    Delete rspace directory

--- rspace/config.js
+++
@@ -1,2 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://CheeseSpread:Abc12e@cluster.dvq2y.mongodb.net/RSpace?retryWrites=true&w=majority')
--- rspace/courses.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const courseSchema = new mongoose.Schema({
-    courseName : String
-});
-
-module.exports = mongoose.model('courses',courseSchema );
